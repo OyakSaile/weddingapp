fc547151416072f8e4a742d381f99d539fd5a588
Kayo Elias <contatodevkayo@gmail.com>
2022-05-18 02:19:08 -0300

    Organizacao de pasta

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./src/screens/home/Home";
+import { theme } from "./src/themes/global";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.primary,
+      }}
+    >
+      <Home />
+      <StatusBar style="auto" backgroundColor="transparent" translucent={true} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/home/Home.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import styles from "./style";
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Oi</Text>
+    </View>
+  );
+}
